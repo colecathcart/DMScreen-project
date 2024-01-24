29b37423301cb5c6fefd6ee6e8e268f76ccaf583
cole.cathcart1 <cole.cathcart1@cs.ucalgary.ca>
2024-01-24 16:48:19 -0700

    Added items and rules to searchbar

--- app/src/components/Searchbar.jsx
+++ app/src/components/Searchbar.jsx
@@ -5,33 +5,53 @@ const Searchbar = ({settheRules}) => {
 
 	const [input, setInput] = useState("")
 	const [spells, setSpells] = useState()
-	const [results, setResults] = useState([])
+	const [rules, setRules] = useState()
+	const [items, setItems] = useState()
+	const [spellresults, setspellResults] = useState([])
+	const [itemresults, setitemResults] = useState([])
+	const [ruleresults, setruleResults] = useState([])
 
 	useEffect(()=>{
-        const FetchAllSpells = async ()=>{
+        const FetchAllResults = async ()=>{
             try {
-                const res = await axios.get("http://localhost:4000/search?url=/api/spells")
-                setSpells(res.data.results)
+                const spellres = await axios.get("http://localhost:4000/search?url=/api/spells")
+				const gruleres = await axios.get("http://localhost:4000/search?url=/api/rule-sections")
+				const condres = await axios.get("http://localhost:4000/search?url=/api/conditions")
+				const equipres = await axios.get("http://localhost:4000/search?url=/api/equipment")
+				const mitemres = await axios.get("http://localhost:4000/search?url=/api/magic-items")
+                setSpells(spellres.data.results)
+				setRules(gruleres.data.results.concat(condres.data.results))
+				setItems(equipres.data.results.concat(mitemres.data.results))
             } catch (error) {
                 console.log(error)
             }
         }
-        FetchAllSpells()
+        FetchAllResults()
     },[])
 
 	const handleSearch = (value) => {
 		setInput(value)
-		const res = spells.filter((item) => {
+		const spellres = spells.filter((item) => {
 			return value && item && item.name && item.name.toLowerCase().includes(value)
 		})
-		setResults(res.slice(0,10))
+		const itemres = items.filter((item) => {
+			return value && item && item.name && item.name.toLowerCase().includes(value)
+		})
+		const ruleres = rules.filter((item) => {
+			return value && item && item.name && item.name.toLowerCase().includes(value)
+		})
+		setspellResults(spellres.slice(0,5))
+		setitemResults(itemres.slice(0,5))
+		setruleResults(ruleres.slice(0,5))
 		//console.log(results)
 	}
 
 	const handleRuleAdd = async (rule) => {
 		try {
 			setInput("")
-			setResults([])
+			setspellResults([])
+			setruleResults([])
+			setitemResults([])
 			const res = await axios.get("http://localhost:4000/search?url="+rule.url)
 			//console.log(res.data)
 			//setRules(prevRules => [...prevRules, {spell : res.data, key : keynum}])
@@ -51,7 +71,16 @@ const Searchbar = ({settheRules}) => {
 			</div>
 			<table className="searchresults">
 				<tbody>
-					{results.map((result, id) => {
+					{spellresults.length ? <th className="searchresult">Spells</th> : null}
+					{spellresults.map((result, id) => {
+						return <tr className="searchresult"><button key={id} onClick={() => handleRuleAdd(result)}>{result.name}</button></tr>
+					})}
+					{itemresults.length ? <th className="searchresult">Items</th> : null}
+					{itemresults.map((result, id) => {
+						return <tr className="searchresult"><button key={id} onClick={() => handleRuleAdd(result)}>{result.name}</button></tr>
+					})}
+					{ruleresults.length ? <th className="searchresult">Rules</th> : null}
+					{ruleresults.map((result, id) => {
 						return <tr className="searchresult"><button key={id} onClick={() => handleRuleAdd(result)}>{result.name}</button></tr>
 					})}
 				</tbody>
