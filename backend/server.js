a519335e254f5eaf1bc6e25c95a8d2f06f3f0c24
cole.cathcart1 <cole.cathcart1@cs.ucalgary.ca>
2024-03-27 16:29:18 -0600

    heroku commit 1

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(cookieParser())
 if(process.env.NODE_ENV === 'production') {
 	app.use(express.static('../app/build'))
 	app.get('*',(req,res) => {
-
+		res.sendFile(path.resolve(__dirname, '..', 'app', 'build', 'index.html'))
 	})
 }
 
